1785e9b4b3072babf218076062e6530c3cc6ba8d
Marija Petrovic <marijap@protonmail.com>
2023-04-21 09:41:46 +0200

    Update run.sh script

--- spark/run.sh
+++ spark/run.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 
-# printf "\nSAVE FILES TO HDFS\n"
-# sudo docker exec -it spark-master ./spark/bin/spark-submit  ../spark/apps/saveData.py 
+printf "\UPLOAD BATCH FILE TO HDFS\n"
+sudo docker exec -it spark-master ./spark/bin/spark-submit ../spark/apps/upload_data.py
 
-# printf "\nPREPROCESSING\n"
-# sudo docker exec -it spark-master ./spark/bin/spark-submit  ../spark/apps/raw.py 
+printf "\nPREPROCESS BATCH DATA\n"
+sudo docker exec -it spark-master ./spark/bin/spark-submit ../spark/apps/preprocessing.py
 
-# printf "\nBATCH PROCESSING\n"
-# sudo docker exec -it spark-master ./spark/bin/spark-submit  --jars ../spark/apps/postgresql-42.5.1.jar ../spark/apps/transformation.py
+printf "\nRUN BATCH PROCESSING\n"
+sudo docker exec -it spark-master ./spark/bin/spark-submit  --jars ../spark/apps/postgresql-42.5.1.jar ../spark/apps/batch_processing.py
 
-printf "\REAL-TIME PROCESSING\n"
-docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/biggestHumidity.py  &
-docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/averageWindSpeed.py & 
-docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/sparkStreaming.py  
+# printf "\RUN REAL-TIME PROCESSING\n"
+# docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/biggestHumidity.py  &
+# docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/averageWindSpeed.py & 
+# docker exec spark-master ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 --jars ./spark/apps/postgresql-42.5.1.jar ./spark/apps/sparkStreaming.py  
